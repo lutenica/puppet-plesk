07162bc130c8da7603a689df2ab04f863b5543c1
lutenica <zidancho@gmail.com>
2022-01-27 16:20:44 +0000

    Added better documentation

--- manifests/extension.pp
+++ manifests/extension.pp
@@ -6,15 +6,25 @@
 #   }
 #
 # @param ensure
-#   Ensure the state.
+#   Ensure the state. Where present and absent
+#   are obvious
+#
+#   latest will upgrade the extension
+#   if installed. If not it will install.
+#
+#   enabled enables if installed, installs if not.
+#  
+#
+#   disable only disables if the extension is installed
+#   and does not uninstall. 
 #
 # @param exec
 #   Script to execute on the extension
 #
 # @url
-#   Url to download extension/upgrade from
+#   Url to download extension/upgrade from  
 #      
-define plesk::extensions(
+define plesk::extension(
   Enum['present', 'absent', 'latest', 'enabled', 'disabled', 'exec'] $ensure = present,
   Optional['String'] $exec,
   Optional['String'] $url
